e81a26844031b2f1d4aa4fd37d550af4469767eb
TomasBelfonte <tomas.belfonte@gmail.com>
2022-11-02 19:10:15 +0100

    Js_FizzBuzz

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ const totCaselle = 10 * 10;
 const squareContainer = document.querySelector(".square");
 
 for (let i = 1; i <= totCaselle; i++) {
-    // squareContainer.innerHTML += `<div class="subSquare bgBlu text-center align-center pt-5 m-3 fw-bold fs-5">${i}</div>`;
 
     if ( i % 3 === 0 && i % 5 === 0) {
         squareContainer.innerHTML += `<div class="subSquare bgRosso text-center align-center pt-5 m-3 fw-bold fs-5"> fizzbuzz </div>`;
